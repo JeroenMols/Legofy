f7594328f805334f37c1b7ff837dbd59c2c0543d
Jeroen Mols <jeroen.mols.be@gmail.com>
2016-03-30 20:59:24 +0200

    Added imageview with non processed image

--- app/src/main/java/com/jeroenmols/legofy/MainActivity.java
+++ app/src/main/java/com/jeroenmols/legofy/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jeroenmols.legofy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView image = (ImageView) findViewById(R.id.imageview_main);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.flower);
+        image.setImageBitmap(bitmap);
     }
 }
 
