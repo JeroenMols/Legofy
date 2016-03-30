37d0cd00a29b430d63a5d8b1511606661c422631
Jeroen Mols <jeroen.mols.be@gmail.com>
2016-03-30 21:31:00 +0200

    Very simple legofy algorithm

--- app/src/main/java/com/jeroenmols/legofy/MainActivity.java
+++ app/src/main/java/com/jeroenmols/legofy/MainActivity.java
@@ -2,6 +2,10 @@ package com.jeroenmols.legofy;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -15,8 +19,29 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView image = (ImageView) findViewById(R.id.imageview_main);
 
+        int granularity = 64;
+
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.flower);
-        image.setImageBitmap(bitmap);
+        Bitmap brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick);
+        Bitmap scaledBrick = Bitmap.createScaledBitmap(brick, granularity, granularity, false);
+
+        int imageWidth = bitmap.getWidth() / granularity;
+        int imageHeight = bitmap.getHeight() / granularity;
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, imageWidth, imageHeight, false);
+
+        Bitmap legofied = Bitmap.createBitmap(imageWidth * granularity, imageHeight * granularity, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(legofied);
+        Paint paint = new Paint();
+
+        for (int i = 0; i < scaledBitmap.getWidth(); i++) {
+            for (int j = 0; j < scaledBitmap.getHeight(); j++) {
+                paint.setColorFilter(new PorterDuffColorFilter(scaledBitmap.getPixel(i, j), PorterDuff.Mode.OVERLAY));
+                canvas.drawBitmap(scaledBrick, i * granularity, j * granularity, paint);
+            }
+        }
+
+
+        image.setImageBitmap(legofied);
     }
 }
 
