d06c19e63ef12b509bd71b99dbf26dd27557cbdf
Jeroen Mols <jeroen.mols.be@gmail.com>
2016-04-12 12:52:10 +0200

    Removed progressbar and button,

--- app/src/main/java/com/jeroenmols/legofy/MainActivity.java
+++ app/src/main/java/com/jeroenmols/legofy/MainActivity.java
@@ -1,41 +1,18 @@
 package com.jeroenmols.legofy;
 
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int GRANULARITY = 64;
+    public static final int ANIM_DURATION_MS = 300;
     private ImageView image;
-    private SeekBar seekbar;
-    private SeekBar.OnSeekBarChangeListener onSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.profile_picture);
-            LegofyDrawable legofyDrawable = new LegofyDrawable(getResources(), bitmap);
-            image.setImageDrawable(legofyDrawable);
-            legofyDrawable.createLegoFiedBitmap(getResources(), seekbar.getProgress(), true);
-        }
-
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
-            //NOP
-        }
-
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
-            //NOP
-        }
-    };
-    ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,21 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         image = (ImageView) findViewById(R.id.imageview_main);
-        seekbar = (SeekBar) findViewById(R.id.seekbar_main_bricksize);
-        seekbar.setProgress(GRANULARITY);
-        seekbar.setOnSeekBarChangeListener(onSeekBarChangeListener);
-        Button button = (Button) findViewById(R.id.button_main_highres);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.profile_picture);
-                LegofyDrawable legofyDrawable = new LegofyDrawable(getResources(), bitmap);
-                image.setImageDrawable(legofyDrawable);
-                legofyDrawable.createLegoFiedBitmap(getResources(), seekbar.getProgress(), false);
-            }
-        });
-
-//        image.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.profile_picture));
 
         addImageClickListener(R.id.imageview_profile, R.drawable.profile_picture);
         addImageClickListener(R.id.imageview_monalise, R.drawable.mona_lisa);
@@ -79,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     private void setImageResource(@DrawableRes int image) {
         EffectDrawable customDrawable = new EffectDrawable(BitmapFactory.decodeResource(getResources(), image));
         this.image.setImageDrawable(customDrawable);
-        customDrawable.applyEffect(new LegofyEffect(getResources(), 64, 300));
+        customDrawable.applyEffect(new LegofyEffect(getResources(), GRANULARITY, ANIM_DURATION_MS));
     }
 }
 
