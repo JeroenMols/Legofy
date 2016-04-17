254a98876f81d4144c9068a163441d67b61a4236
Jeroen Mols <jeroen.mols.be@gmail.com>
2016-04-17 16:50:55 +0200

    Added NoEffect drawable

--- app/src/main/java/com/jeroenmols/brickeffect/NoEffect.java
+++ app/src/main/java/com/jeroenmols/brickeffect/NoEffect.java
@@ -1,7 +1,30 @@
 package com.jeroenmols.brickeffect;
 
+import android.graphics.Bitmap;
+
 /**
  * @author Jeroen Mols on 17/04/16.
  */
-public class NoEffect {
+public class NoEffect implements Effect {
+    private Bitmap bitmap;
+
+    @Override
+    public void initialize(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
+
+    @Override
+    public boolean hasNextFrame() {
+        return false;
+    }
+
+    @Override
+    public int getFrameDuration() {
+        return 100000000;
+    }
+
+    @Override
+    public Bitmap nextFrame() {
+        return bitmap;
+    }
 }
