6ed151562d520c25ed09d7baa204c9df13807411
Jeroen Mols <jeroen.mols.be@gmail.com>
2016-04-23 17:45:04 +0200

    Added DrawingWrapper to do the actual drawing (logic that cannot be tested)

--- library/src/main/java/com/jeroenmols/legofy/DrawingWrapper.java
+++ library/src/main/java/com/jeroenmols/legofy/DrawingWrapper.java
@@ -1,7 +1,34 @@
 package com.jeroenmols.legofy;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+
 /**
  * @author Jeroen Mols on 23/04/16.
  */
 public class DrawingWrapper {
+
+    private final Paint paint;
+    private final Canvas canvas;
+    private Bitmap brickBitmap;
+
+    public DrawingWrapper() {
+        paint = new Paint();
+        canvas = new Canvas();
+    }
+
+    public void setBitmap(Resources resources, Bitmap targetBitmap, int brickSize) {
+        canvas.setBitmap(targetBitmap);
+        brickBitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(resources, R.drawable.brick), brickSize, brickSize, false);
+    }
+
+    public void drawBrick(Bitmap bitmap, int color, int xPos, int yPos, int width) {
+        paint.setColorFilter(new PorterDuffColorFilter(color, PorterDuff.Mode.OVERLAY));
+        canvas.drawBitmap(brickBitmap, xPos * brickBitmap.getWidth(), yPos * brickBitmap.getHeight(), paint);
+    }
 }
