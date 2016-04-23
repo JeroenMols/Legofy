7884a7fb0ec89e86fd3ae60e6b1ee278cb174210
Jeroen Mols <jeroen.mols.be@gmail.com>
2016-04-23 16:25:42 +0200

    Process image

--- library/src/main/java/com/jeroenmols/legofy/Legofy.java
+++ library/src/main/java/com/jeroenmols/legofy/Legofy.java
@@ -1,7 +1,12 @@
 package com.jeroenmols.legofy;
 
+import android.graphics.Bitmap;
+
 /**
  * @author Jeroen Mols on 23/04/16.
  */
 public class Legofy {
+    public Bitmap processBitmap(Bitmap bitmap) {
+        return bitmap;
+    }
 }
--- library/src/test/java/com/jeroenmols/legofy/LegofyTest.java
+++ library/src/test/java/com/jeroenmols/legofy/LegofyTest.java
@@ -1,8 +1,12 @@
 package com.jeroenmols.legofy;
 
+import android.graphics.Bitmap;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -13,5 +17,14 @@ public class LegofyTest {
     @Test
     public void canInstantiate() throws Exception {
         new Legofy();
+    }
+
+    @Test
+    public void processBitmap() throws Exception {
+        Bitmap mockBitmap = Mockito.mock(Bitmap.class);
+
+        Bitmap processedBitmap = new Legofy().processBitmap(mockBitmap);
+
+        assertNotNull(processedBitmap);
     }
 }
