80e7ad22ac82eea9e33cc80283569b23ce97b146
Jeroen Mols <jeroen.mols.be@gmail.com>
2016-04-23 16:11:10 +0200

    Added empty library

---
+++ library/src/main/java/com/jeroenmols/legofy/Legofy.java
@@ -0,0 +1,7 @@
+package com.jeroenmols.legofy;
+
+/**
+ * @author Jeroen Mols on 23/04/16.
+ */
+public class Legofy {
+}
---
+++ library/src/test/java/com/jeroenmols/legofy/LegofyTest.java
@@ -0,0 +1,15 @@
+package com.jeroenmols.legofy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class LegofyTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
