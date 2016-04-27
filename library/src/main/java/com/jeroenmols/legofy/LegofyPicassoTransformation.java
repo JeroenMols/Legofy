7228159115a36a1e0438393638c6b08e5a1f2d7d
Jeroen Mols <jeroen.mols.be@gmail.com>
2016-04-27 21:36:06 +0200

    Added LegofyPicassoTransformation

---
+++ library/src/main/java/com/jeroenmols/legofy/LegofyPicassoTransformation.java
@@ -0,0 +1,7 @@
+package com.jeroenmols.legofy;
+
+/**
+ * @author Jeroen Mols on 27/04/16.
+ */
+public class LegofyPicassoTransformation {
+}
---
+++ library/src/test/java/com/jeroenmols/legofy/LegofyPicassoTransformationTest.java
@@ -0,0 +1,19 @@
+package com.jeroenmols.legofy;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Test;
+
+/**
+ * @author Jeroen Mols on 27/04/16.
+ */
+@SmallTest
+public class LegofyPicassoTransformationTest {
+
+    @Test
+    public void canInstantiate() throws Exception {
+        new LegofyPicassoTransformation();
+    }
+
+
+}
