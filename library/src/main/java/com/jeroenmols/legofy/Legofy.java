baf81b84df6672a75d9bb8bc7b7e1b987dac54e7
Jeroen Mols <jeroen.mols.be@gmail.com>
2016-04-27 09:32:20 +0200

    Refactored code

--- library/src/main/java/com/jeroenmols/legofy/Legofy.java
+++ library/src/main/java/com/jeroenmols/legofy/Legofy.java
@@ -41,34 +41,41 @@ public class Legofy {
     }
 
     public Bitmap convert(Bitmap bitmap) {
-        float scaleFactor = createScaleFactor(bitmap);
+        float scaleFactor = getScaleFactor(bitmap);
         int brickSize = (int) (bitmap.getWidth() * scaleFactor) / bricksInWidth;
 
-        Bitmap processedBitmap = createBitmapForBrickSize(bitmap, brickSize);
-        brickDrawer.setBitmap(context.getResources(), processedBitmap, brickSize);
+        Bitmap outputBitmap = createBitmapForBrickSize(bitmap, brickSize);
+        brickDrawer.setBitmap(context.getResources(), outputBitmap, brickSize);
+        drawAllBricks(bitmap, brickSize, outputBitmap);
 
-        int amountOfBricks = processedBitmap.getWidth() * processedBitmap.getHeight() / brickSize / brickSize;
-        Bitmap scaledBitmap = bitmapWrapper.createScaledBitmap(bitmap, bricksInWidth, amountOfBricks / bricksInWidth, true);
+        return outputBitmap;
+    }
+
+    private void drawAllBricks(Bitmap outputBitmap, int brickSize, Bitmap processedBitmap) {
+        int amountOfBricks = getAmountOfBricks(brickSize, processedBitmap);
+        Bitmap scaledBitmap = bitmapWrapper.createScaledBitmap(outputBitmap, bricksInWidth, amountOfBricks / bricksInWidth, true);
         for (int i = 0; i < amountOfBricks; i++) {
             int posX = i % bricksInWidth;
             int posY = i / bricksInWidth;
-
-            brickDrawer.drawBrick(scaledBitmap.getPixel(posX, posY), posX * brickSize, posY * brickSize);
+            int color = scaledBitmap.getPixel(posX, posY);
+            brickDrawer.drawBrick(color, posX * brickSize, posY * brickSize);
         }
-
-        return processedBitmap;
     }
 
-    private Bitmap createBitmapForBrickSize(Bitmap bitmap, int brickSize) {
-        int width = brickSize * bricksInWidth;
-        int height = ((int) (bitmap.getHeight() * createScaleFactor(bitmap)) / brickSize) * brickSize;
-        return bitmapWrapper.createBitmap(width, height, Bitmap.Config.ARGB_4444);
+    private int getAmountOfBricks(int brickSize, Bitmap processedBitmap) {
+        return processedBitmap.getWidth() * processedBitmap.getHeight() / brickSize / brickSize;
     }
 
-    private float createScaleFactor(Bitmap bitmap) {
+    private float getScaleFactor(Bitmap bitmap) {
         float scaleX = ((float) DEFAULT_MAXOUTPUTSIZE) / bitmap.getWidth();
         float scaleY = ((float) DEFAULT_MAXOUTPUTSIZE) / bitmap.getHeight();
         float scale = Math.min(scaleX, scaleY);
         return Math.min(scale, DEFAULT_SCALE);
+    }
+
+    private Bitmap createBitmapForBrickSize(Bitmap bitmap, int brickSize) {
+        int width = brickSize * bricksInWidth;
+        int height = ((int) (bitmap.getHeight() * getScaleFactor(bitmap)) / brickSize) * brickSize;
+        return bitmapWrapper.createBitmap(width, height, Bitmap.Config.ARGB_4444);
     }
 }
